ecdd8a3408597d2f6fcc925a7e991807beebac94
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-06-12 14:59:24 +0900

    리눅스 스터디 과제 진행중

--- src/linuxEx220612/myList3/Node.java
+++ src/linuxEx220612/myList3/Node.java
@@ -4,16 +4,19 @@ public class Node {
 
     int value;
     Node next = null;
+    Node cur = new Node(3);
 
     public Node(int value) {
         this.value = value;
     }
 
     public void add(int value) {
+        this.value = value;
         this.next = new Node(value);
     }
 
     public void show() {
-        System.out.println(this.next.value);
+        System.out.println(this.cur.value);
+        this.cur = this.next;
     }
 }
