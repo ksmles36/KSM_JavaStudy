cf3062b15b91ad449baae60fc8fe3c338fec4899
ksmles36 <ksmles36@gmail.com>
2022-06-16 10:42:50 +0900

    220616

--- src/javaCollections_2/LinkedHashSetTest.java
+++ src/javaCollections_2/LinkedHashSetTest.java
@@ -1,6 +1,7 @@
 package javaCollections_2;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashSet;
 
 public class LinkedHashSetTest {
@@ -10,21 +11,26 @@ public class LinkedHashSetTest {
         HashSet<String> hashSet = new HashSet<>();
         LinkedHashSet<String> linkedHashSet = new LinkedHashSet();
 
+        hashSet.add("5");
+        hashSet.add("5");
+        hashSet.add("6");
+        hashSet.add("7");
+        hashSet.remove("6");
         hashSet.add("3");
-        hashSet.add("3");
-        hashSet.add("2");
-        hashSet.add("1");
-        hashSet.remove("2");
-        hashSet.add("4");
 
+        linkedHashSet.add("5");
+        linkedHashSet.add("5");
+        linkedHashSet.add("6");
+        linkedHashSet.add("7");
+        linkedHashSet.remove("6");
         linkedHashSet.add("3");
-        linkedHashSet.add("3");
-        linkedHashSet.add("2");
-        linkedHashSet.add("1");
-        linkedHashSet.remove("2");
-        linkedHashSet.add("4");
 
         hashSet.forEach(e -> System.out.println("hashSet = " + e));
         linkedHashSet.forEach(e -> System.out.println("linkedHashSet = " + e));
+
+        Iterator<String> iterator = linkedHashSet.iterator();
+        while (iterator.hasNext()) {
+            System.out.println("iterator 사용 가능 : " + iterator.next());
+        }
     }
 }
