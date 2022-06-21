12b89d7a3481844999bf8175b99a3a0d2f1e013e
ksmles36 <ksmles36@gmail.com>
2022-06-22 08:30:25 +0900

    220622

--- src/echo/EchoClient.java
+++ src/echo/EchoClient.java
@@ -1,41 +1,52 @@
 package echo;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
 import java.util.Scanner;
 
 public class EchoClient {
 
     public static void main(String[] args) {
 
-        connectEchoServer("localhost", 8000);
+        connectEchoServer("121.167.147.22", 8000);
+
     }
 
     private static void connectEchoServer(String host, int port) {
         try {
-            Socket clientSocket = new Socket(host, port);
+            SocketChannel clientSocket = SocketChannel.open();
+            clientSocket.configureBlocking(true);
+            clientSocket.connect(new InetSocketAddress(host, port));
             System.out.println("서버에 연결됨");
 
-            PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-
             Scanner sc = new Scanner(System.in);
+
+            ByteBuffer sendByteBuffer;
+            ByteBuffer receiveByteBuffer;
+            Charset charset = Charset.forName("UTF-8");
 
             while(true){
                 System.out.print("to Server : ");
                 String inputData = sc.nextLine();
+
                 if(inputData.trim().equalsIgnoreCase("exit")) {
                     clientSocket.close();
                     System.out.println("서버 연결 종료");
                     break;
                 }
 
-                pw.print(inputData);
-                pw.flush();
+                sendByteBuffer = charset.encode(inputData);
+                clientSocket.write(sendByteBuffer);
 
-                System.out.println("from Server : " + br.readLine());
+                receiveByteBuffer = ByteBuffer.allocate(1024);
+                clientSocket.read(receiveByteBuffer);
+                receiveByteBuffer.flip();
+
+                String receiveStr = charset.decode((receiveByteBuffer)).toString();
+
+                System.out.println("from Server : " + receiveStr);
             }
         } catch (Exception e) {
             e.printStackTrace();
