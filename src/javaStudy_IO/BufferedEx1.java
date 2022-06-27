5b8c6e141d3c77ebb9830e3a85d3934fb396bdd0
ksmles36 <ksmles36@gmail.com>
2022-06-27 22:28:33 +0900

    220627

---
+++ src/javaStudy_IO/BufferedEx1.java
@@ -0,0 +1,46 @@
+package javaStudy_IO;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class BufferedEx1 {
+
+    public static void main(String[] args) {
+
+        notUseBuffered();
+        useBuffered();
+
+    }
+
+    private static void notUseBuffered() {
+        try {
+            FileInputStream fis1 = new FileInputStream("C:\\Users\\kim\\Pictures\\SampleJPGImage_1MB.jpg");
+            long start = System.currentTimeMillis();
+            while (fis1.read() != -1) {
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("버퍼를 사용하지 않았을 때 : " + (end-start) + "ms 소요");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void useBuffered() {
+        try {
+            FileInputStream fis2 = new FileInputStream("C:\\Users\\kim\\Pictures\\SampleJPGImage_5MB.jpg");
+            BufferedInputStream br = new BufferedInputStream(fis2);
+            long start = System.currentTimeMillis();
+            while (br.read() != -1) {
+            }
+            long end = System.currentTimeMillis();
+            System.out.println("버퍼를 사용했을 때 : " + (end-start) + "ms 소요");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
