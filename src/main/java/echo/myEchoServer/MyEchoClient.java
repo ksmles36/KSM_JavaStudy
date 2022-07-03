c48a077dac1193b29c67ac5f6d8aa668caddfba5
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-07-03 22:22:18 +0900

    220703

---
+++ src/main/java/echo/myEchoServer/MyEchoClient.java
@@ -0,0 +1,55 @@
+package echo.myEchoServer;
+
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+public class MyEchoClient {
+
+    public static void main(String[] args) {
+
+        connectEchoServer("localhost", 9001);
+
+    }
+
+    private static void connectEchoServer(String host, int port) {
+        try {
+            SocketChannel channel = SocketChannel.open();
+            channel.configureBlocking(true);
+            channel.connect(new InetSocketAddress(host, port));
+            System.out.println("서버에 연결됨");
+
+            Scanner sc = new Scanner(System.in);
+
+            ByteBuffer byteBuffer;
+            Charset charset = Charset.forName("UTF-8");
+
+            while(true){
+                System.out.print("to Server : ");
+                String inputData = sc.nextLine();
+
+                if(inputData.trim().equalsIgnoreCase("exit")) {
+                    channel.close();
+                    System.out.println("서버 연결 종료");
+                    break;
+                }
+
+                byteBuffer = charset.encode(inputData);
+                channel.write(byteBuffer);
+
+                byteBuffer = ByteBuffer.allocate(128);
+                channel.read(byteBuffer);
+                byteBuffer.flip();
+
+                String receiveStr = charset.decode((byteBuffer)).toString();
+
+                System.out.println("from Server : " + receiveStr);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ src/main/java/echo/myEchoServer/MyEchoServer.java
@@ -0,0 +1,59 @@
+package echo.myEchoServer;
+
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MyEchoServer {
+
+    int port;
+    int backlogSize;
+    ServerSocket serverSocket;
+
+    public MyEchoServer(int port, int backlogSize) {
+        this.port = port;
+        this.backlogSize = backlogSize;
+    }
+
+    private MyEchoServer bind() {
+        try {
+            serverSocket = new ServerSocket();
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(port);
+            serverSocket.bind(inetSocketAddress, backlogSize);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return this;
+    }
+
+    private MyEchoServer listen() {
+        try {
+            System.out.println(String.format("Listen %d ...", port));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return this;
+    }
+
+    private void accept() {
+        for (; ; ) {
+            try {
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("clientSocket = " + clientSocket);
+                new MyEchoThread(port).start();
+                break;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new MyEchoServer(9001, 1024).bind().listen().accept();
+    }
+
+}
