3709f77af888c267df5413cace846015a9fe8cb1
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-07-03 22:51:09 +0900

    220703

--- src/main/java/linuxEx220612/myMap2/Main.java
+++ src/main/java/linuxEx220612/myMap2/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        MyMap myMap = new MyMap();
 
+        myMap.put("1","aaa");
+        myMap.put("2", "bbb");
+        myMap.put("1","ccc");
+
+        System.out.println(myMap.get("1"));
+
+        myMap.replace("1" , "ggg");
+        myMap.show();
 
     }
 }
--- src/main/java/linuxEx220612/myMap2/MyMap.java
+++ src/main/java/linuxEx220612/myMap2/MyMap.java
@@ -2,6 +2,74 @@ package linuxEx220612.myMap2;
 
 public class MyMap {
 
+        private String key;
+        private String value;
+        private MyMap next;
+        private MyMap back;
 
+        public MyMap() {
+            key = null;
+            value = null;
+            next = null;
+            back = null;
+        }
+
+        public MyMap(String key, String value){
+            this.key = key;
+            this.value = value;
+            this.next = null;
+            this.back = null;
+        }
+
+        public MyMap put(String key, String value) {
+            if (this.key == key) {
+                System.out.println("이미 있는 key 값 입니다.");
+                return this;
+            }
+            if (this.key == null) {
+                this.key = key;
+                this.value = value;
+                return this;
+            } else {
+                if (next == null) {
+                    next = new MyMap(key, value);
+                    next.back = this;
+                } else {
+                    next.put(key, value);
+                }
+                return this;
+            }
+        }
+
+        public String get(String key){
+            if(this.key.equals(key)){
+                return this.value;
+            }else{
+                if (next == null) return null;
+                else return next.get(key);
+            }
+        }
+
+        public MyMap replace(String value, String reValue){
+            if(this.key.equals(value)){
+                this.value = reValue;
+                return this;
+            }else {
+                if(next == null) return null;
+                else return next.replace(value,reValue);
+            }
+        }
+
+        public MyMap show() {
+            if(value == null) {
+                System.out.println("리스트가 비어있음");
+                return this;
+            }
+
+            System.out.println("Key : " + this.key +" Value : "+this.value);
+
+            if(this.next == null) return this;
+            else return next.show();
+        }
 
 }
