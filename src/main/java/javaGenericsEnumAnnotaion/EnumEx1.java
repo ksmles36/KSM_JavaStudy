86b2b1f547081529cf38d24a8ae6abd4419c0369
Kim Seongmin <45448156+ksmles36@users.noreply.github.com>
2022-07-10 23:10:43 +0900

    220710

---
+++ src/main/java/javaGenericsEnumAnnotaion/EnumEx1.java
@@ -0,0 +1,25 @@
+package javaGenericsEnumAnnotaion;
+
+public enum EnumEx1 {
+
+    EAST(1, "동쪽"),
+    WEST(2, "서쪽"),
+    SOUTH(3, "남쪽"),
+    NORTH(4, "북쪽");
+
+    private int value;
+    private String direction;
+
+    EnumEx1(int value, String direction) {
+        this.value = value;
+        this.direction = direction;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+}
---
+++ src/main/java/javaGenericsEnumAnnotaion/EnumEx1Main.java
@@ -0,0 +1,14 @@
+package javaGenericsEnumAnnotaion;
+
+public class EnumEx1Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(EnumEx1.EAST.getDirection());
+        System.out.println(EnumEx1.WEST.getDirection());
+        System.out.println(EnumEx1.SOUTH.getDirection());
+        System.out.println(EnumEx1.NORTH.getDirection());
+
+    }
+
+}
